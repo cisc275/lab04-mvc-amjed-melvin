fc595958b64aabafd6fa230326f246104e40f014
ahallak <ahallak@udel.edu>
2019-03-04 18:42:42 -0500

    added blank methods

--- Model.java
+++ Model.java
@@ -8,3 +8,58 @@
  * provide direction
  * provide location
  **/
+
+public class Model {
+	int width;
+	int height;
+	int imageWidth;
+	int imageHeight;
+	
+	Model(int width, int height, int imageWidth, int imageHeight) {
+		this.width = width;
+		this.height = height;
+		this.imageWidth = imageWidth;
+		this.imageHeight = imageHeight;
+	}
+	//TODO
+	public int getX() {return 0;}
+	//TODO
+	public int getY() {return 0;}
+	//TODO
+	int getDirect(){return 0;}
+	//TODO
+	void updateLocationAndDirection() {}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getImageWidth() {
+		return imageWidth;
+	}
+
+	public void setImageWidth(int imageWidth) {
+		this.imageWidth = imageWidth;
+	}
+
+	public int getImageHeight() {
+		return imageHeight;
+	}
+
+	public void setImageHeight(int imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+	
+}
--- View.java
+++ View.java
@@ -7,3 +7,51 @@
  * use proper images for direction
  * load images for all direction (an image should only be loaded once!!! why?)
  **/
+
+
+public class View {
+	int width;
+	int height;
+	int imageWidth;
+	int imageHeight;
+	
+	//TODO + change params
+	public void update(int x, int y, Object direct) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getImageWidth() {
+		return imageWidth;
+	}
+
+	public void setImageWidth(int imageWidth) {
+		this.imageWidth = imageWidth;
+	}
+
+	public int getImageHeight() {
+		return imageHeight;
+	}
+
+	public void setImageHeight(int imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+
+	
+}
